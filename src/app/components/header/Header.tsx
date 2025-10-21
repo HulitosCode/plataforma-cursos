0361fa60e5ccf3891ae388af2c69de1e470903e7
HulitosCode <heltonmatimbe8@gmail.com>
2025-10-21 15:30:56 +0200

    feat: ajustando e criando header responsivo e formatando codigo

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,27 +1,44 @@
-'use client'
+"use client";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { Menu } from "lucide-react";
 
 export default function Header() {
-
-    const currentPath = usePathname()
+  const currentPath = usePathname();
 
   return (
-    <nav className="flex items-center justify-center bg-green-700">
-      <ul className="flex p-4 gap-10">
-        <li>
-          <Link href="/" data-active={currentPath === '/'} className='data-[active=true]:underline'>Pagina Inicial</Link>
+    <nav className="flex items-center justify-start md:justify-center bg-green-700 py-2 sm:py-4 px-6">
+      <button className="sm:hidden">
+        <Menu />
+      </button>
+      <ul className="flex gap-10">
+        <li className="hidden sm:block">
+          <Link
+            href="/"
+            data-active={currentPath === "/"}
+            className="data-[active=true]:underline"
+          >
+            Pagina Inicial
+          </Link>
         </li>
-        <li>
-          <Link href="/cursos" data-active={currentPath === '/cursos'} className='data-[active=true]:underline'>Cursos</Link>
+        <li className="hidden sm:block">
+          <Link
+            href="/cursos"
+            data-active={currentPath === "/cursos"}
+            className="data-[active=true]:underline"
+          >
+            Cursos
+          </Link>
         </li>
-        <li>
+        <li className="hidden sm:block">
           <Link href="/blog" target="_blanck">
             Blog de Noticias
           </Link>
         </li>
       </ul>
+
+      <h1 className="sm:hidden">Tayspark</h1>
     </nav>
   );
 }
